fc03891df8b7746697a86434e4ac3cf1e8072de6
Leu Family <romanianfobjr@gmail.com>
2021-11-11 12:46:05 -0600

    mentee_intake table current stage finished

--- data/migrations/20211111164717_mentee_intake.js
+++ data/migrations/20211111164717_mentee_intake.js
@@ -11,6 +11,16 @@ exports.up = function (knex) {
         .inTable('application_tickets')
         .onDelete('RESTRICT')
         .onUpdate('RESTRICT');
+      table.string('email').notNullable();
+      table.string('location').notNullable();
+      table.string('name').notNullable();
+      table.boolean('lives_in_US').notNullable();
+      table.boolean('formerly_incarcerated').notNullable();
+      table.string('list_convictions');
+      table.string('tech_stack').notNullable();
+      table.string('experience_level').notNullable();
+      table.string('your_hope').notNullable();
+      table.string('other_info');
     });
 };
 
