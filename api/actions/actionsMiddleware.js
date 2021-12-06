dec0ff81f034d6301ee9bc2b84beaba05370d6dd
Rick <ricksmycodeguy@gmail.com>
2021-12-06 12:31:24 -0600

    addes subjectBody validation middleware

--- api/actions/actionsMiddleware.js
+++ api/actions/actionsMiddleware.js
@@ -1,8 +1,14 @@
 // const subject = required('./actionsModel/');
 
-// const subjectBody = (req, res, next) => {
-//     const {
-//         subjectBody
-//     } = req.body;
-//     if(!"" || subject.trim === '')
-// }
+const subjectBody = (req, res, next) => {
+  const { issue } = req.body;
+  if (!issue)
+    return next({
+      status: 400,
+      message: 'You must submit an issue as a string of text',
+    });
+  next();
+};
+module.exports = {
+  subjectBody,
+};
