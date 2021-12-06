2dbc085e6cb63619990db1fbc99c2ad2dcaa74d1
Rick <ricksmycodeguy@gmail.com>
2021-12-06 14:14:25 -0600

    adds put req to router in actionsRouter.js

--- api/actions/actionsRouter.js
+++ api/actions/actionsRouter.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { validateSubjectBody } = require('./actionsMiddleware');
 // const authRequired = require('../middleware/authRequired');
 const Actions = require('./actionsModel');
 const router = express.Router();
@@ -12,4 +13,36 @@ router.get('/', function (req, res) {
       res.status(500).json({ message: err.message });
     });
 });
+
+router.post('/', validateSubjectBody, (req, res, next) => {
+  const action = req.body;
+  Actions.Create(action)
+    .then(() => {
+      res.status(201).json({ message: 'success', action });
+    })
+    .catch(next);
+});
+
+router.put('/:id', validateSubjectBody, (req, res, next) => {
+  const issue = req.params.subject_id;
+  const changes = req.body;
+  Actions.Update(issue, changes)
+    .then((change) => {
+      if (change) {
+        Actions.findById(issue).then((success) => {
+          res.status(200).json({
+            message: `Action '${success.subject_id}' 
+            updated`,
+            success,
+          });
+        });
+      }
+    })
+    .catch(next);
+});
+
+router.delete('/:id', (req, res) => {
+  res.json('delete wired/fired');
+});
+
 module.exports = router;
