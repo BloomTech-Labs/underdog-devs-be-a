5a0abbc5ccd8b41db14688db74ae3a722d7bc824
courtneyharris <harriscourtney98@gmail.com>
2021-12-07 17:39:52 -0500

    Added a mentee progression table

--- data/migrations/20211207221814_mentee_progression.js
+++ data/migrations/20211207221814_mentee_progression.js
@@ -1,7 +1,23 @@
-// exports.up = function(knex) {
+exports.up = function (knex) {
+  return knex.schema
+    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    .createTable('mentee_progression', function (table) {
+      table
+        .increments('mentee_progression_id')
+        .notNullable()
+        .unique()
+        .primary();
+      table
+        .string('profile_id')
+        .references('profile_id')
+        .inTable('profiles')
+        .onUpdate('RESTRICT')
+        .onDelete('RESTRICT')
+        .notNullable();
+      table.string('progress').notNullable();
+    });
+};
 
-// };
-
-// exports.down = function(knex) {
-
-// };
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('mentee_progression');
+};
