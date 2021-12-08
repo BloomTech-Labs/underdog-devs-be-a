516ed158d7f5fbf0f2f7c4772c21a1ffb1a52c84
courtneyharris <harriscourtney98@gmail.com>
2021-12-08 15:41:53 -0500

    Progression router recieves profileId and responds with mentee progress status

--- api/progession/progressionModel.js
+++ api/progession/progressionModel.js
@@ -1,10 +1,15 @@
 const db = require('../../data/db-config');
 
 const findByMenteeId = (profile_id) => {
-  return db('mentee_progression')
-    .where({ profile_id })
+  return db('profiles')
+    .leftJoin(
+      'mentee_progression',
+      'profiles.progress_id',
+      'mentee_progression.progress_id'
+    )
+    .where('profiles.profile_id', profile_id)
     .first()
-    .select('progress');
+    .select('mentee_progression.progress');
 };
 
 module.exports = { findByMenteeId };
