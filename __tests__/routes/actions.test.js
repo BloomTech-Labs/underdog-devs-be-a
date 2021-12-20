f8ef19ca5078d3e1e2ea4597423e1920faf28f9d
Rick <ricksmycodeguy@gmail.com>
2021-12-20 08:12:08 -0600

    adds describe block to test  GET /actions/:id

--- __tests__/routes/actions.test.js
+++ __tests__/routes/actions.test.js
@@ -28,13 +28,32 @@ describe('actions router endpoints', () => {
       actionsModel.findAll.mockResolvedValue([]);
       const res = await request(server).get('/actions');
       expect(res.status).toBe(200);
+      expect(res.body.length).toBe(0);
+      expect(actionsModel.findAll.mock.calls.length).toBe(1);
     });
 
     it('should return 500 when encountering an unexpected condition', async () => {
-      //attempts to stub an error
       sinon.stub(actionsModel, 'findAll').throws(Error('query failed'));
       await request(app) //passes Express app to supertest
         .get('/actions')
+        .expect(500);
+    });
+  });
+
+  describe('GET /actions/:id', () => {
+    it('should return 200 when profile found', async () => {
+      actionsModel.findById.mockResolvedValue({
+        id: 'd376de0577681ca93614',
+      });
+      const res = await request(server).get('/actions/d376de0577681ca93614');
+
+      expect(res.status).toBe(200);
+      expect(actionsModel.findById.mock.calls.length).toBe(1);
+    });
+    it('should return 500 when encountering an unexpected condition', async () => {
+      sinon.stub(actionsModel, 'findById').throws(Error('query failed'));
+      await request(app) //passes Express app to supertest
+        .get('/actions/:id')
         .expect(500);
     });
   });
