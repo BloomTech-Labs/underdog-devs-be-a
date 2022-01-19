b0923b89f38c725432bece16a925bf88ae322b9e
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2022-01-18 21:48:52 -0800

    credit to these awesome folks
    Co-authored-by: kennedybryant <kennedy_marisa96@yahoo.com>
    Co-authored-by: krueng <krueng@gmail.com>

--- __tests__/routes/applications.test.js
+++ __tests__/routes/applications.test.js
@@ -33,24 +33,50 @@ describe('application router endpoints', () => {
       const res = await request(server).get('/application');
 
       expect(res.status).toBe(200);
+      expect(res.body.length).toBe(0);
+      expect(Application.getPendingTickets.mock.calls.length).toBe(1);
     });
   });
 
   describe('GET /application/:role', () => {
+    Application.getPendingTicketsByRole.mockResolvedValue({
+      profile_id: '00ulzfj6nX79gu0Nh5d6',
+      first_name: 'User',
+      last_name: '6',
+      role_name: 'mentor',
+      created_at: '2022-01-13T20:44:31.827Z',
+      application_id: 5,
+    });
     test('returns 200 OK', async () => {
-      Application.getPendingTicketsByRole.mockResolvedValue([]);
-      const res = await request(server).get('/application/mentee');
+      const res = await request(server).get('/application/mentor');
 
       expect(res.status).toBe(200);
+    });
+    test('application with requested role is returned', async () => {
+      const res = await request(server).get('/application/mentor');
+
+      expect(res.body.role_name).toBe('mentor');
     });
   });
 
   describe('GET /application/id/:id', () => {
+    Application.getTicketById.mockResolvedValue({
+      profile_id: '00u13oned0U8XP8Mb4x7',
+      first_name: 'User',
+      last_name: '8',
+      role_name: 'mentee',
+      created_at: '2022-01-13T20:44:31.827Z',
+      application_id: 2,
+    });
     test('returns 200 OK', async () => {
-      Application.getTicketById.mockResolvedValue([]);
-      const res = await request(server).get('/application/mentee');
+      const res = await request(server).get('/application/id/2');
 
       expect(res.status).toBe(200);
+    });
+    test('application_id matches requested id', async () => {
+      const res = await request(server).get('/application/id/2');
+
+      expect(res.body.application_id).toBe(2);
     });
   });
 });
