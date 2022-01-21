ce3f990079973c5db0c6c0be6862d5e52e7e6229
Lance Turbes <50921397+lanceturbes@users.noreply.github.com>
2022-01-21 10:40:01 -0800

    Adds 'resource' schema and schema folder
    Co-authored-by: Brandon May <battleb2014@gmail.com>

---
+++ api/schemas/resourceSchema.js
@@ -0,0 +1,34 @@
+const yup = require('yup');
+
+const resourceSchema = yup.object().shape({
+  resource_id: yup
+    .mixed()
+    .oneOf([undefined], "'resource_id' must not be provided"),
+  created_at: yup
+    .mixed()
+    .oneOf([undefined], "'created_at' field must not be provided"),
+  updated_at: yup
+    .mixed()
+    .oneOf([undefined], "'updated_at' field must not be provided"),
+  resource_name: yup
+    .string()
+    .trim()
+    .required("'resource_name' is required")
+    .min(3, "'resource_name' must contain at least 3 characters")
+    .max(100, "'resource_name' must contain less than 100 characters"),
+  category: yup.string().trim().required("'category' is required"),
+  condition: yup.string().trim().required("'condition' is required"),
+  assigned: yup.boolean(),
+  current_assignee: yup
+    .string()
+    .trim()
+    .required("'current_assignee' ID is required"),
+  previous_assignee: yup
+    .string()
+    .trim()
+    .required("'previous_assignee' ID is required"),
+  monetary_value: yup.string().trim(),
+  deductible_donation: yup.boolean(),
+});
+
+module.exports = resourceSchema;
