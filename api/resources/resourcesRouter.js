9ebf744ac3c344efdb97f4191344fc5c83cc4632
Lance Turbes <50921397+lanceturbes@users.noreply.github.com>
2022-01-21 16:25:36 -0800

    Feature: Search for Resources Via Filter (#33)

--- api/resources/resourcesRouter.js
+++ api/resources/resourcesRouter.js
@@ -120,16 +120,33 @@ const { adminRequired } = require('../middleware/permissionsRequired');
  *      '403':
  *        $ref: '#/components/responses/UnauthorizedError'
  */
-router.get('/', authRequired, (req, res) => {
-  Resources.findAll()
-    .then((resources) => {
-      res.status(200).json(resources);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err.message });
-    });
-});
 
+router.get('/', authRequired, async (req, res, next) => {
+  try {
+    const filters = req.query;
+    const allResources = await Resources.findAll();
+
+    if (!filters) {
+      return res.status(200).json(allResources);
+    }
+
+    const filteredResources = allResources.filter((resource) => {
+      let resourceIsValid = true;
+      for (let key in filters) {
+        resourceIsValid =
+          resourceIsValid &&
+          (resource[key] === filters[key] ||
+            resource[key].toLowerCase().includes(filters[key].toLowerCase()));
+      }
+      return resourceIsValid;
+    });
+
+    return res.status(200).json(filteredResources);
+  } catch (err) {
+    return next(err);
+  }
+});
+    
 /**
  * @swagger
  * /resources/{resource_id}:
