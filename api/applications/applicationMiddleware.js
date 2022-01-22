661ab6f0cb273d1b84e676cff7a2e0311c33df84
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2022-01-21 16:56:56 -0800

    separated middlewares and updated error message

--- api/applications/applicationMiddleware.js
+++ api/applications/applicationMiddleware.js
@@ -4,24 +4,31 @@ const {
   getMenteeIntake,
 } = require('./applicationModel');
 
-const checkRole = async (req, res, next) => {
-  const profileExists = await getTicketById(req.params.id);
-  try {
-    if (!profileExists) {
-      next({ status: 400, message: 'Invalid profile_id' });
+const validateProfile = (req, res, next) => {
+  getTicketById(req.params.id).then((profile) => {
+    if (profile) {
+      next(profile);
+    } else {
+      next({ status: 404, message: `profile_id ${req.params.id} not found` });
     }
-    if (profileExists.role_name === 'mentor') {
-      const mentorData = await getMentorIntake(profileExists.profile_id);
+  });
+};
+
+const checkRole = async (req, res, next) => {
+  const validatedProfile = req.body;
+  try {
+    if (validatedProfile.role_name === 'mentor') {
+      const mentorData = await getMentorIntake(validatedProfile.profile_id);
       next(mentorData);
-    } else if (profileExists.role_name === 'mentee') {
-      const menteeData = await getMenteeIntake(profileExists.profile_id);
+    } else if (req.body.role_name === 'mentee') {
+      const menteeData = await getMenteeIntake(validatedProfile.profile_id);
       next(menteeData);
     } else {
-      next(profileExists);
+      next(validatedProfile);
     }
   } catch (err) {
     next(err);
   }
 };
 
-module.exports = { checkRole };
+module.exports = { validateProfile, checkRole };
