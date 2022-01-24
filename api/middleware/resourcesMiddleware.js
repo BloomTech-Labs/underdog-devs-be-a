8245353d79fea9ae0e8388d8768d2c5457612736
Lance Turbes <50921397+lanceturbes@users.noreply.github.com>
2022-01-24 13:26:49 -0800

    Adds 'checkResourceIdExists' middleware

--- api/middleware/resourcesMiddleware.js
+++ api/middleware/resourcesMiddleware.js
@@ -1,4 +1,18 @@
+const Resource = require('../resources/resourcesModel');
 const resourceSchema = require('../../data/schemas/resourceSchema');
+
+async function checkResourceIdExists(req, res, next) {
+  const { resource_id } = req.params;
+  try {
+    await Resource.findByResourceId(resource_id);
+    return next();
+  } catch (err) {
+    return next({
+      status: 404,
+      message: `resource with ID ${resource_id} not found`,
+    });
+  }
+}
 
 async function validateResource(req, res, next) {
   try {
@@ -14,4 +28,4 @@ async function validateResource(req, res, next) {
   }
 }
 
-module.exports = { validateResource };
+module.exports = { checkResourceIdExists, validateResource };
