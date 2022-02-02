a7eb672e7719c1da816b9ebaab6ca35663bf36b6
Lance Turbes <50921397+lanceturbes@users.noreply.github.com>
2022-02-02 14:46:35 -0800

    test: check for input errors to [POST] /profiles

--- __tests__/routes/profiles.test.js
+++ __tests__/routes/profiles.test.js
@@ -134,25 +134,27 @@ describe('Profile Router', () => {
   });
 
   describe('[POST] /profile', () => {
+    const postNewUser = (userInfo) =>
+      request(app).post('/profile').send(userInfo);
+
     describe('success', () => {
       let res;
       beforeAll(async () => {
-        const validReqBody = {
+        res = await postNewUser({
           profile_id: 'bradbowman',
           first_name: 'Brad',
           last_name: 'Bowman',
           email: 'brad.bowman@maildrop.cc',
           role_id: 5,
-        };
-        res = await request(app).post('/profile').send(validReqBody);
+        });
       });
 
       it('requires authentication', () => {
         expect(authRequired).toBeCalled();
       });
 
-      it('responds with status 200', async () => {
-        const expected = 200;
+      it('responds with status 201', async () => {
+        const expected = 201;
         const actual = res.status;
 
         expect(actual).toBe(expected);
@@ -179,6 +181,196 @@ describe('Profile Router', () => {
         const actual = res.body.profile;
 
         expect(actual).toMatchObject(expected);
+      });
+    });
+
+    describe('failure', () => {
+      describe('missing profile_id', () => {
+        let res;
+        beforeAll(async () => {
+          res = await postNewUser({
+            first_name: 'Firstname',
+            last_name: 'Lastname',
+            email: 'realemail@maildrop.cc',
+          });
+        });
+
+        it('responds with status 400', () => {
+          const expected = 400;
+          const actual = res.status;
+
+          expect(actual).toBe(expected);
+        });
+
+        it('returns message "profile_id is required"', () => {
+          const expected = /profile_id is required/i;
+          const actual = res.body.message;
+
+          expect(actual).toMatch(expected);
+        });
+      });
+
+      describe('invalid first name', () => {
+        describe('missing', () => {
+          let res;
+          beforeAll(async () => {
+            res = await postNewUser({
+              profile_id: '9000',
+              last_name: 'Lastname',
+              email: 'realemail@maildrop.cc',
+            });
+          });
+
+          it('responds with status 400', () => {
+            const expected = 400;
+            const actual = res.status;
+
+            expect(actual).toBe(expected);
+          });
+
+          it('returns message "first_name is required"', () => {
+            const expected = /first_name is required/i;
+            const actual = res.body.message;
+
+            expect(actual).toMatch(expected);
+          });
+        });
+
+        describe('too long', () => {
+          const tooLongFirstName =
+            'Lorem ipsum dolor sit amet lorem, consectetur porttitor.';
+          let res;
+          beforeAll(async () => {
+            res = await postNewUser({
+              profile_id: '9000',
+              first_name: tooLongFirstName,
+              last_name: 'Lastname',
+              email: 'realemail@maildrop.cc',
+            });
+          });
+
+          it('responds with status 400', () => {
+            const expected = 400;
+            const actual = res.status;
+
+            expect(actual).toBe(expected);
+          });
+
+          it('returns message "first_name must be between 2-50 chars"', () => {
+            const expected = /first_name must be between 2-50 char/i;
+            const actual = res.body.message;
+
+            expect(actual).toMatch(expected);
+          });
+        });
+      });
+
+      describe('invalid last name', () => {
+        describe('missing', () => {
+          let res;
+          beforeAll(async () => {
+            res = await postNewUser({
+              profile_id: '9000',
+              first_name: 'Firstname',
+              email: 'realemail@maildrop.cc',
+            });
+          });
+
+          it('responds with status 400', () => {
+            const expected = 400;
+            const actual = res.status;
+
+            expect(actual).toBe(expected);
+          });
+
+          it('returns message "last_name is required"', () => {
+            const expected = /last_name is required/i;
+            const actual = res.body.message;
+
+            expect(actual).toMatch(expected);
+          });
+        });
+
+        describe('too long', () => {
+          const tooLongLastName =
+            'Lorem ipsum dolor sit amet, consectetur massa nunc.';
+          let res;
+          beforeAll(async () => {
+            res = await postNewUser({
+              profile_id: '9000',
+              first_name: 'Firstname',
+              last_name: tooLongLastName,
+              email: 'realemail@maildrop.cc',
+            });
+          });
+
+          it('responds with status 400', () => {
+            const expected = 400;
+            const actual = res.status;
+
+            expect(actual).toBe(expected);
+          });
+
+          it('returns message "last_name must be between 2-50 chars"', () => {
+            const expected = /last_name must be between 2-50 char/i;
+            const actual = res.body.message;
+
+            expect(actual).toMatch(expected);
+          });
+        });
+      });
+
+      describe('invalid email', () => {
+        describe('missing', () => {
+          let res;
+          beforeAll(async () => {
+            res = await postNewUser({
+              profile_id: '9000',
+              first_name: 'Firstname',
+              last_name: 'Lastname',
+            });
+          });
+
+          it('responds with status 400', () => {
+            const expected = 400;
+            const actual = res.status;
+
+            expect(actual).toBe(expected);
+          });
+
+          it('returns message "email is required"', () => {
+            const expected = /email is required/i;
+            const actual = res.body.message;
+
+            expect(actual).toMatch(expected);
+          });
+        });
+
+        describe('misformatted', () => {
+          let res;
+          beforeAll(async () => {
+            res = await postNewUser({
+              profile_id: '9000',
+              first_name: 'Firstname',
+              last_name: 'Lastname',
+              email: 'invalid-email',
+            });
+          });
+
+          it('responds with status 400', () => {
+            const expected = 400;
+            const actual = res.status;
+
+            expect(actual).toBe(expected);
+          });
+
+          it('returns message "email must be validly formatted"', () => {
+            const expected = /email must be validly formatted/i;
+            const actual = res.body.message;
+
+            expect(actual).toMatch(expected);
+          });
+        });
       });
     });
   });
