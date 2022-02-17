b74d877c7f4d5c8b4eacd84af2091a7a7d813249
onuri5 <jomibaofficial@gmail.com>
2022-02-16 20:07:35 -0600

    updated mentor_intake to reflect final question alteration

--- data/migrations/20211111164703_mentor_intake.js
+++ data/migrations/20211111164703_mentor_intake.js
@@ -12,19 +12,23 @@ exports.up = function (knex) {
         .onDelete('RESTRICT')
         .onUpdate('RESTRICT');
       table.string('email').notNullable();
-      table.string('location').notNullable();
+      table.string('country').notNullable();
+      table.string('city').notNullable();
+      table.string('state').notNullable();
       table.string('first_name').notNullable();
       table.string('last_name').notNullable();
       table.string('current_comp');
-      table.string('other_tech');
       table.boolean('front_end').defaultTo(false);
       table.boolean('back_end').defaultTo(false);
       table.boolean('full_stack').defaultTo(false);
       table.boolean('ux_design').defaultTo(false);
       table.boolean('android_mobile').defaultTo(false);
       table.boolean('ios_mobile').defaultTo(false);
+      table.boolean('career_development').defaultTo(false);
       table.string('experience_level').notNullable();
-      table.string('mentor_commitment', 255).notNullable();
+      table.boolean('industry_knowledge').defaultTo(false);
+      table.boolean('job_search').defaultTo(false);
+      table.boolean('pair_programming').defaultTo(false);
       table.string('other_info', 255);
     });
 };
