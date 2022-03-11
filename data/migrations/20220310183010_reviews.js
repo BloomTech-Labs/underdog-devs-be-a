a55f95870efa9d041c4fd9a63cc6470ead6f3e39
SheebaHashmi <sheebahashmi0@gmail.com>
2022-03-11 16:21:24 -0500

    Fix syntax

--- data/migrations/20220310183010_reviews.js
+++ data/migrations/20220310183010_reviews.js
@@ -1,30 +1,28 @@
 exports.up = function (knex) {
-    return knex.schema
-        .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
-        .createTable('reviews', function (table) {
-            table.increments('review_id').notNullable().unique().primary();
-            table.string('review').notNullable();
-            table
-                .string('mentor_id')
-                .unsigned()
-                .notNullable()
-                .references('profile_id')
-                .inTable('profiles')
-                .onDelete('CASCADE')
-                .onUpdate('CASCADE');
-            table
-                .string('mentee_id')
-                .unsigned()
-                .notNullable()
-                .references('profile_id')
-                .inTable('profiles')
-                .onDelete('CASCADE')
-                .onUpdate('CASCADE');
-
-        });
-
+  return knex.schema
+    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    .createTable('reviews', function (table) {
+      table.increments('review_id').notNullable().unique().primary();
+      table.string('review').notNullable();
+      table
+        .string('mentor_id')
+        .unsigned()
+        .notNullable()
+        .references('profile_id')
+        .inTable('profiles')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      table
+        .string('mentee_id')
+        .unsigned()
+        .notNullable()
+        .references('profile_id')
+        .inTable('profiles')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+    });
 };
 
 exports.down = function (knex) {
-    return knex.schema.dropTableIfExists('reviews');
+  return knex.schema.dropTableIfExists('reviews');
 };
--- data/seeds/014-reviews.js
+++ data/seeds/014-reviews.js
@@ -5,10 +5,24 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('reviews').insert([
-        {review_id: 1, review: 'positive',mentor_id:'00u13oned0U8XP8Mb4x7',mentee_id:12},
-        {review_id: 2, review: 'negative',mentor_id:'00u13oned0U8XP8Mb4x7',mentee_id:10},
-        {review_id: 3, review: 'positive',mentor_id:'00u13omswyZM1xVya4x7',mentee_id:7}
-
+        {
+          review_id: 1,
+          review: 'positive',
+          mentor_id: '00u13oned0U8XP8Mb4x7',
+          mentee_id: 12,
+        },
+        {
+          review_id: 2,
+          review: 'negative',
+          mentor_id: '00u13oned0U8XP8Mb4x7',
+          mentee_id: 10,
+        },
+        {
+          review_id: 3,
+          review: 'positive',
+          mentor_id: '00u13omswyZM1xVya4x7',
+          mentee_id: 7,
+        },
       ]);
     });
 };
