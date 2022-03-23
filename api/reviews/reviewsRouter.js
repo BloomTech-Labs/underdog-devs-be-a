649943ed99c8cbb77e1da8e772a97aa29f64a792
Christian <christiansmith2394@gmail.com>
2022-03-23 12:27:13 -0400

    Laying out endpoints/middleware for posting reviews to mentees

--- api/reviews/reviewsRouter.js
+++ api/reviews/reviewsRouter.js
@@ -44,13 +44,30 @@ router.get('/mentee/:id', authRequired, validProfileID, (req, res) => {
       if (reviews) {
         res.status(200).json(reviews);
       } else {
-        res.status(404).json({ error: 'Reviews Not Found, Check mentor ID' });
+        res.status(404).json({ error: 'Reviews Not Found, Check mentee ID' });
       }
     })
     .catch((err) => {
       res.status(500).json({ error: err.message });
     });
 });
+
+// Create a review for a mentee
+
+router.post(
+  '/',
+  authRequired,
+  validNewReview,
+  adminRequired,
+  (req, res, next) => {
+    const review = req.body;
+    Review.Create(review)
+      .then(() => {
+        res.status(201).json({ message: 'success' });
+      })
+      .catch(next);
+  }
+);
 
 ////////////////MIDDLEWARE////////////////
 
@@ -69,6 +86,27 @@ function validProfileID(req, res, next) {
       }
     })
     .catch(next);
+}
+
+// Validate new review includes a mentor and mentee
+
+function validNewReview(req, res, next) {
+  const assign = req.body;
+  if (!assign) {
+    res.status(400).json({
+      message: 'Missing Assignment Data',
+    });
+  } else if (!assign.mentor_id) {
+    res.status(400).json({
+      message: 'Missing mentor_id field',
+    });
+  } else if (!assign.mentee_id) {
+    res.status(400).json({
+      message: 'Missing mentee_id field',
+    });
+  } else {
+    next();
+  }
 }
 
 module.exports = router;
