21d7ad052bcb1ac5c8130afbe49b6dbd4a97a59b
SheebaHashmi <sheebahashmi0@gmail.com>
2022-03-24 13:53:21 -0400

    Created migrations for tickets table

---
+++ data/migrations/20220324171955_tickets.js
@@ -0,0 +1,53 @@
+exports.up = function (knex) {
+  return knex.schema
+    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    .createTable('tickets', function (table) {
+      table.increments('ticket_id').notNullable().unique().primary();
+      table
+        .integer('ticket_type')
+        .unsigned()
+        .notNullable()
+        .references('ticket_type_id')
+        .inTable('ticket_types')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      table.string('ticket_status').notNullable();
+      table.string('ticket_subject').notNullable();
+      table
+        .string('requested_for')
+        .unsigned()
+        .references('profile_id')
+        .inTable('profiles')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      table
+        .string('submitted_by')
+        .unsigned()
+        .notNullable()
+        .references('profile_id')
+        .inTable('profiles')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      table
+        .string('approved_by')
+        .unsigned()
+        .references('profile_id')
+        .inTable('profiles')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      table.boolean('urgent').default(false);
+      table.string('notes');
+      table
+        .integer('requested_role')
+        .unsigned()
+        .references('role_id')
+        .inTable('roles')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      table.timestamps(true, true);
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('tickets');
+};
