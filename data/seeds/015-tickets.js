f22b6d6fcf0ba3cb647b3b41914352622b7bd477
SheebaHashmi <sheebahashmi0@gmail.com>
2022-03-28 18:14:33 -0400

    Fixed tickets seed to have integer as ticket_type

--- data/seeds/015-tickets.js
+++ data/seeds/015-tickets.js
@@ -1,20 +1,20 @@
 const tickets = [
   {
-    ticket_type: '1',
+    ticket_type: 1,
     ticket_status: 'pending',
     ticket_subject: 'Spencer missed his 2nd weekly session, may be dropped?',
     requested_for: '10',
     submitted_by: '7',
   },
   {
-    ticket_type: '2',
+    ticket_type: 2,
     ticket_status: 'approved',
     ticket_subject: 'Application for mentee',
     requested_role: 4,
     submitted_by: '7',
   },
   {
-    ticket_type: '1',
+    ticket_type: 1,
     ticket_status: 'pending',
     ticket_subject:
       "My mentor isn't really helping me learn, could I seek reassignment?",
@@ -22,21 +22,21 @@ const tickets = [
     submitted_by: '11',
   },
   {
-    ticket_type: '2',
+    ticket_type: 2,
     ticket_status: 'pending',
     ticket_subject: 'Application for mentee',
     requested_role: 4,
     submitted_by: '12',
   },
   {
-    ticket_type: '2',
+    ticket_type: 2,
     ticket_status: 'approved',
     ticket_subject: 'Application for mentor',
     requested_role: 3,
     submitted_by: '9',
   },
   {
-    ticket_type: '3',
+    ticket_type: 3,
     ticket_status: 'pending',
     ticket_subject:
       'Elon deserves to have the 2020 macbook pro. Of all the mentees I have, I think he has the most potential.',
@@ -44,7 +44,7 @@ const tickets = [
     submitted_by: '7',
   },
   {
-    ticket_type: '2',
+    ticket_type: 2,
     ticket_status: 'pending',
     ticket_subject: 'Application for mentor',
     requested_role: 3,
@@ -52,7 +52,7 @@ const tickets = [
     submitted_by: '00u13omswyZM1xVya4x7',
   },
   {
-    ticket_type: '3',
+    ticket_type: 3,
     ticket_status: 'pending',
     ticket_subject:
       'Foo needs the Computer Monitor, because he has bad eyesight.',
@@ -60,7 +60,7 @@ const tickets = [
     submitted_by: '9',
   },
   {
-    ticket_type: '4',
+    ticket_type: 4,
     ticket_status: 'approved',
     ticket_subject: 'Applying for admin role',
     requested_role: '1',
