af2fc88fdb530e4d8f92b0f0a27b7bf2c271bc3a
beefybroccoli <tedkimemail@yahoo.com>
2022-03-31 09:38:46 -0700

    added expected failed cases to delete request - id not found

--- __tests__/routes/note.test.js
+++ __tests__/routes/note.test.js
@@ -229,82 +229,138 @@ describe('Notes Router', () => {
     });
   });
 
-  // describe('[PUT] /notes', () => {
-  //   describe('succeed', () => {
-  //     let resPost, resPut;
-  //     beforeAll(async () => {
-  //       //step 1 - post a new note
-  //       resPost = await request(app).post('/notes').send({
-  //         content_type: 'type a',
-  //         content: 'some text here',
-  //         level: 'low',
-  //         visible_to_admin: true,
-  //         visible_to_mentor: true,
-  //         mentor_id: '00u13omswyZM1xVya4x7',
-  //         mentee_id: '00u13oned0U8XP8Mb4x7',
-  //       });
+  describe('[PUT] /notes', () => {
+    describe('succeed', () => {
+      let resPost, resPut;
+      beforeAll(async () => {
+        //step 1 - post a new note
+        resPost = await request(app).post('/notes').send({
+          content_type: 'type a',
+          status: 'in progress',
+          content: 'some text here',
+          level: 'low',
+          visible_to_admin: true,
+          visible_to_mentor: true,
+          mentor_id: '00u13omswyZM1xVya4x7',
+          mentee_id: '00u13oned0U8XP8Mb4x7',
+        });
 
-  //       //step 2 - put the note
-  //       resPut = await request(app)
-  //         .put(`/notes/${resPost.body[0].note_id}`)
-  //         .send({
-  //           content: 'new content',
-  //         });
-  //     });
+        //step 2 - put the note
+        resPut = await request(app)
+          .put(`/notes/${resPost.body[0].note_id}`)
+          .send({
+            content: 'new content',
+          });
+      });
 
-  //     it('requires authentication', () => {
-  //       expect(authRequired).toBeCalled();
-  //     });
+      it('requires authentication', () => {
+        expect(authRequired).toBeCalled();
+      });
 
-  //     //step 3 - assertion
-  //     it('content to match new content', async () => {
-  //       console.log(resPost.body[0].note_id);
-  //       const expected = /new content/;
-  //       const actual = resPut.text;
-  //       expect(actual).toMatch(expected);
-  //     });
-  //   });
-  //   describe('failed', () => {
-  //     expect(1).toBe(2);
-  //   });
-  // });
+      //step 3 - assertion
+      it('content to match new content', async () => {
+        console.log(resPost.body[0].note_id);
+        const expected = /new content/;
+        const actual = resPut.text;
+        expect(actual).toMatch(expected);
+      });
+    });
+    describe('failed', () => {
+      //--------------------------------------------------
+      describe('case - invalid note_id', () => {
+        let resPost, resPut;
+        beforeAll(async () => {
+          //step 1 - post a new note
+          resPost = await request(app).post('/notes').send({
+            content_type: 'type a',
+            status: 'in progress',
+            content: 'some text here',
+            level: 'low',
+            visible_to_admin: true,
+            visible_to_mentor: true,
+            mentor_id: '00u13omswyZM1xVya4x7',
+            mentee_id: '00u13oned0U8XP8Mb4x7',
+          });
 
-  // describe('[DELETE] /notes', () => {
-  //   describe('succeed', () => {
-  //     let resPost, resDelete;
-  //     beforeAll(async () => {
-  //       //step 1 - post a new note
-  //       resPost = await request(app).post('/notes').send({
-  //         content_type: 'type a',
-  //         content: 'some text here',
-  //         level: 'low',
-  //         visible_to_admin: true,
-  //         visible_to_moderator: true,
-  //         visible_to_mentor: true,
-  //         mentor_id: '00u13omswyZM1xVya4x7',
-  //         mentee_id: '00u13oned0U8XP8Mb4x7',
-  //       });
+          //step 2 - put the note
+          resPut = await request(app).put(`/notes/9999999`).send({
+            content: 'new content',
+          });
+        });
 
-  //       //step 2 - put the note
-  //       resDelete = await request(app).delete(
-  //         `/notes/${resPost.body[0].note_id}`
-  //       );
-  //     });
+        it('requires authentication', () => {
+          expect(authRequired).toBeCalled();
+        });
 
-  //     it('requires authentication', () => {
-  //       expect(authRequired).toBeCalled();
-  //     });
+        //step 3 - assertion
+        it('content to match new content', async () => {
+          console.log(resPost.body[0].note_id);
+          const expected = /id does not exist/i;
+          const actual = resPut.body.message;
+          expect(actual).toMatch(expected);
+        });
+      });
+      //--------------------------------------------------
+      describe('case - when the user submit the same content???', () => {
+        it.skip('(need description)', () => {});
+      });
+    });
+  });
 
-  //     //step 3 - assertion
-  //     it('content to match new content', async () => {
-  //       console.log(resPost.body[0].note_id);
-  //       const expected = 200;
-  //       const actual = resDelete.status;
-  //       expect(actual).toBe(expected);
-  //     });
-  //   });
-  //   describe('failed', () => {
-  //     expect(1).toBe(2);
-  //   });
-  // });
+  describe('[DELETE] /notes', () => {
+    describe('succeed', () => {
+      let resPost, resDelete;
+      beforeAll(async () => {
+        //step 1 - post a new note
+        resPost = await request(app).post('/notes').send({
+          content_type: 'type a',
+          content: 'some text here',
+          status: 'in progress',
+          level: 'low',
+          visible_to_admin: true,
+          visible_to_moderator: true,
+          visible_to_mentor: true,
+          mentor_id: '00u13omswyZM1xVya4x7',
+          mentee_id: '00u13oned0U8XP8Mb4x7',
+        });
+
+        //step 2 - put the note
+        resDelete = await request(app).delete(
+          `/notes/${resPost.body[0].note_id}`
+        );
+      });
+
+      it('requires authentication', () => {
+        expect(authRequired).toBeCalled();
+      });
+
+      //step 3 - assertion
+      it('content to match new content', async () => {
+        console.log(resPost.body[0].note_id);
+        const expected = 200;
+        const actual = resDelete.status;
+        expect(actual).toBe(expected);
+      });
+    });
+    describe('failed', () => {
+      describe('case - not id not found', () => {
+        let resDelete;
+        beforeAll(async () => {
+          //step 2 - put the note
+          resDelete = await request(app).delete(`/notes/99999999`);
+        });
+
+        it('requires authentication', () => {
+          expect(authRequired).toBeCalled();
+        });
+
+        //step 3 - assertion
+        it('content to match new content', async () => {
+          const expected = /id does not exist/i;
+          const actual = resDelete.body.message;
+          expect(actual).toMatch(expected);
+        });
+      });
+    });
+  });
 });
