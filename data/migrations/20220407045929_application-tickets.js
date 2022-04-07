0030a2d1310df07e4bdae34f41d1f9eebc32a1c5
david fletcher <davidfletcheraaron@gmail.com>
2022-04-07 01:52:15 -0400

    seed and migration added back in as well

---
+++ data/migrations/20220407045929_application-tickets.js
@@ -0,0 +1,29 @@
+exports.up = function (knex) {
+   return knex.schema
+     .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+     .createTable('application_tickets', function (table) {
+       table.increments('application_id').notNullable().unique().primary();
+       table
+         .integer('position')
+         .unsigned()
+         .notNullable()
+         .references('role_id')
+         .inTable('roles')
+         .onDelete('RESTRICT')
+         .onUpdate('RESTRICT');
+       table
+         .string('profile_id')
+         .notNullable()
+         .references('profile_id')
+         .inTable('profiles')
+         .onDelete('RESTRICT')
+         .onUpdate('RESTRICT');
+       table.boolean('approved').notNullable().defaultTo(false);
+       table.timestamps(true, true);
+       table.string('application_notes').defaultTo('');
+     });
+ };
+ 
+ exports.down = function (knex) {
+   return knex.schema.dropTableIfExists('application_tickets');
+ };
