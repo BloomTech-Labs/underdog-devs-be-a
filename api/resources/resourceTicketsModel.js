071a42fe15cd5114d20edd0bfa0bc1dc4495d2ea
SheebaHashmi <sheebahashmi0@gmail.com>
2022-04-07 13:04:46 -0400

    Removes extra ticket routers and updates App.js

--- api/resources/resourceTicketsModel.js
+++
@@ -1,1 +0,0 @@
-//Todo: Refactor these function to be fetching data from tickets table instead of resource-tickets table
--- api/roleTickets/roleTicketsModel.js
+++
@@ -1,1 +0,0 @@
-// TODO: Refactor these function to fetch data from tickets table instead of role_tickets table
