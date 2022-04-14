61f0c9e94f3518e7737726c94b024fc7125ec60b
Claire List <claire.list2501@gmail.com>
2022-04-14 16:09:58 -0600

    get mentor, mentee, ticket intake built

--- api/applications/applicationModel.js
+++ api/applications/applicationModel.js
@@ -1,2 +1,29 @@
 // TODO: Refactor these function to fetch data from tickets table instead of application_tickets table
 //FIRST, let's dig into the database wooo
+const db = require('../../data/db-config');
+
+const getTicketById = async (ticket_id) => {
+  return db.select('*').from('tickets').where({ ticket_id }).first();
+};
+
+const getMentorIntake = async (mentor_intake_id) => {
+  return db
+    .select('*')
+    .from('mentor_intake')
+    .where({ mentor_intake_id })
+    .first();
+};
+
+const getMenteeIntake = async (mentee_intake_id) => {
+  return db
+    .select('*')
+    .from('mentee_intake')
+    .where({ mentee_intake_id })
+    .first();
+};
+
+module.exports = {
+  getTicketById,
+  getMentorIntake,
+  getMenteeIntake,
+};
