d717022183a52b500d224b16faf9c26a888b5233
DeQuavion Wilburn <qua.thesage19@gmail.com>
2022-04-15 16:49:28 -0500

    fleshed out role ticket model

--- api/roleTickets/roleTicketsModel.js
+++ api/roleTickets/roleTicketsModel.js
@@ -1,1 +1,35 @@
 // TODO: Refactor these function to fetch data from tickets table instead of role_tickets table
+const db = require('../../data/db-config');
+
+const findAllTickets = () => {
+  return db('tickets');
+};
+
+const findByTicketStatus = (ticket_status) => {
+  return db('tickets').where({ ticket_status });
+};
+
+const findTicketById = (ticket_id) => {
+  return db('tickets').where({ ticket_id });
+};
+
+const findByRequestedRole = (requested_role) => {
+  return db('tickets').where({ requested_role });
+};
+
+const updateTicketStatus = (ticket_id, ticket_status) => {
+  return db('tickets')
+    .where({ ticket_id })
+    .update({ ticket_status })
+    .then(() => {
+      return findTicketById(ticket_id);
+    });
+};
+
+module.exports = {
+  findAllTickets,
+  findTicketById,
+  findByTicketStatus,
+  updateTicketStatus,
+  findByRequestedRole,
+};
