cafd5b99f16708a0fac720a88be0ef8e1f3ae5e1
DeQuavion Wilburn <qua.thesage19@gmail.com>
2022-04-18 15:34:55 -0500

    updated model

--- api/roleTickets/roleTicketsModel.js
+++ api/roleTickets/roleTicketsModel.js
@@ -1,22 +1,32 @@
 // TODO: Refactor these function to fetch data from tickets table instead of role_tickets table
 const db = require('../../data/db-config');
 
-const findAllTickets = () => {
-  return db('tickets');
+// This function allows you to see what tickets are requesting a mentee or mentor role via ticket type
+const findAllMenteeOrMentorRoleTickets = () => {
+  return db('tickets as t').select('*').where('t.ticket_type', '=', '2');
 };
 
+// This function allows you to see what tickets are requesting a admin role via ticket type
+const findAllAdminRoleTickets = () => {
+  return db('tickets as t').select('*').where('t.ticket_type', '=', '4');
+};
+
+// Allows you to see tickets by ticket_status
 const findByTicketStatus = (ticket_status) => {
-  return db('tickets').where({ ticket_status });
+  return db('tickets').select('*').where({ ticket_status });
 };
 
-const findTicketById = (ticket_id) => {
-  return db('tickets').where({ ticket_id });
-};
-
+// This function is for searching for requested roles
 const findByRequestedRole = (requested_role) => {
   return db('tickets').where({ requested_role });
 };
 
+// Find ticket by id
+const findTicketById = (ticket_id) => {
+  return db('tickets').where({ ticket_id }).first();
+};
+
+// This function allows you to update ticket status
 const updateTicketStatus = (ticket_id, ticket_status) => {
   return db('tickets')
     .where({ ticket_id })
@@ -27,7 +37,8 @@ const updateTicketStatus = (ticket_id, ticket_status) => {
 };
 
 module.exports = {
-  findAllTickets,
+  findAllMenteeOrMentorRoleTickets,
+  findAllAdminRoleTickets,
   findTicketById,
   findByTicketStatus,
   updateTicketStatus,
