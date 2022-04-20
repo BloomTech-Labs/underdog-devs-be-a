ffb8c25b2c74d094c22c3052da0431fc942b45b3
Claire List <claire.list2501@gmail.com>
2022-04-20 11:56:35 -0600

    deprecate local roleTicketsModel to not conflict w PR

--- api/roleTickets/roleTicketsModel.js
+++ api/roleTickets/roleTicketsModel.js
@@ -1,10 +1,1 @@
 // TODO: Refactor these function to fetch data from tickets table instead of role_tickets table
-
-const db = require('../../data/db-config');
-
-const findByRoleTicketById = async (id) => {
-  const res = await db.select('*').from('tickets').where({ id }).first();
-  return res;
-};
-
-module.exports = { findByRoleTicketById };
