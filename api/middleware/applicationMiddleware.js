7531824a8f3dd5164c01db14d40687373d86cda6
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-07-19 18:11:57 -0700

    added dates to created and updated at

--- api/middleware/applicationMiddleware.js
+++ api/middleware/applicationMiddleware.js
@@ -18,6 +18,8 @@ const cacheSignUpData = async (req, res, next) => {
   const role = req.params.role;
   const formData = req.body;
   const sharedFields = {
+    created_at: Date.now(),
+    updated_at: Date.now(),
     profile_id: formData.profile_id,
     first_name: formData.first_name,
     last_name: formData.last_name,
