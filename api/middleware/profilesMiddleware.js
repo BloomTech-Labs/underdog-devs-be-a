486a5b412468f6a3083964ccfe244ae67c94e1fc
Melody McClure <melodylayne79@gmail.com>
2022-08-08 15:19:33 -0500

    added new middlware and router functions, we are pretty sure this is the right setup but need to test more thoroughly to be sure.

--- api/middleware/profilesMiddleware.js
+++ api/middleware/profilesMiddleware.js
@@ -1,4 +1,5 @@
 const { create } = require('../profile/profileModel');
+const axios = require('axios');
 
 const createProfile = async (req, res, next) => {
   const tempProfileId = Math.random().toString(36).slice(-8);
@@ -20,4 +21,26 @@ const createProfile = async (req, res, next) => {
   }
 };
 
-module.exports = { createProfile };
+const checkAvailability = (req, res, next) => {
+  const current_role = req.body.role_id;
+  const profile = req.body.profile_id;
+  let status = req.body.accepting_new_mentees;
+
+  axios
+    .post(`${process.env.DS_API_URL}/update/${current_role}/${profile}`)
+    .then((res) => {
+      //Not sure if this logic belongs in the axios call or if it should be written beforehand, ie. on line 28 of the file.
+      if (current_role === 'mentor') {
+        status = !status;
+      }
+      next({ status: res.status, message: res.data });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+module.exports = {
+  createProfile,
+  checkAvailability,
+};
