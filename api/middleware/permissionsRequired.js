dae38a5af1b58ffc2a22fb7f78d0cedde56b6891
Mike Ferrete <m.ferrete88@gmail.com>
2022-08-18 18:46:38 -0800

    commented out body of all middleware in file. This will allow access to the app during the okta/auth0 transition

--- api/middleware/permissionsRequired.js
+++ api/middleware/permissionsRequired.js
@@ -1,54 +1,57 @@
-const Profiles = require('../profile/profileModel');
-const jwt_decode = require('jwt-decode');
+// const Profiles = require('../profile/profileModel');
+// const jwt_decode = require('jwt-decode');
 
 const superAdminRequired = async (req, res, next) => {
-  const token = req.headers.authorization;
-  const user = jwt_decode(token);
-  await Profiles.findById(user.sub)
-    .then((selectedUser) => {
-      if (selectedUser.role_id == 1) {
-        next();
-      } else {
-        next({
-          status: 401,
-          message: 'invalid credentials / not superadmin',
-        });
-      }
-    })
-    .catch(next);
+  // const token = req.headers.authorization;
+  // const user = jwt_decode(token);
+  // await Profiles.findById(user.sub)
+  //   .then((selectedUser) => {
+  //     if (selectedUser.role_id == 1) {
+  //       next();
+  //     } else {
+  //       next({
+  //         status: 401,
+  //         message: 'invalid credentials / not superadmin',
+  //       });
+  //     }
+  //   })
+  //   .catch(next);
+  next();
 };
 
 const adminRequired = (req, res, next) => {
-  const token = req.headers.authorization;
-  const user = jwt_decode(token);
+  // const token = req.headers.authorization;
+  // const user = jwt_decode(token);
 
-  Profiles.findById(user.sub)
-    .then((selectedUser) => {
-      if (selectedUser.role_id <= 2) next();
-      else
-        next({
-          status: 401,
-          message: 'invalid credentials / not admin',
-        });
-    })
-    .catch(next);
+  // Profiles.findById(user.sub)
+  //   .then((selectedUser) => {
+  //     if (selectedUser.role_id <= 2) next();
+  //     else
+  //       next({
+  //         status: 401,
+  //         message: 'invalid credentials / not admin',
+  //       });
+  //   })
+  //   .catch(next);
+  next();
 };
 
 const mentorRequired = async (req, res, next) => {
-  const token = req.headers.authorization;
-  const user = jwt_decode(token);
-  await Profiles.findById(user.sub)
-    .then((selectedUser) => {
-      if (selectedUser.role_id <= 3) {
-        next();
-      } else {
-        next({
-          status: 401,
-          message: 'invalid credentials / not mentor',
-        });
-      }
-    })
-    .catch(next);
+  // const token = req.headers.authorization;
+  // const user = jwt_decode(token);
+  // await Profiles.findById(user.sub)
+  //   .then((selectedUser) => {
+  //     if (selectedUser.role_id <= 3) {
+  //       next();
+  //     } else {
+  //       next({
+  //         status: 401,
+  //         message: 'invalid credentials / not mentor',
+  //       });
+  //     }
+  //   })
+  //   .catch(next);
+  next();
 };
 
 module.exports = {
