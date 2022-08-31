6b035ff01602f112dada54b1c1739ece6a167a02
enhopkin@gmail.com <enhopkin@gmail.com>
2022-08-31 10:38:21 -0600

    removed unused endpoints and commented out code, updated swagger comments

--- api/applications/applicationRouter.js
+++ api/applications/applicationRouter.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const authRequired = require('../middleware/authRequired');
+// const authRequired = require('../middleware/authRequired');
 const Application = require('./applicationModel');
 const router = express.Router();
-const { adminRequired } = require('../middleware/permissionsRequired.js');
+// const { adminRequired } = require('../middleware/permissionsRequired.js');
 const {
   cacheSignUpData,
-  checkApplicationExists,
-  checkRole,
   sendData,
 } = require('../middleware/applicationMiddleware');
 const { createProfile } = require('../middleware/profilesMiddleware');
@@ -20,207 +18,94 @@ const { baseURL } = require('../../config/dsConfig');
  * @swagger
  * components:
  *  schemas:
- *    Application:
- *      type: object
- *      required:
- *        - position
- *        - profile_id
- *      properties:
- *        application_id:
- *          type: integer
- *          description: Unique primary key referencing the application's numeric ID
- *        position:
- *          type: integer
- *          description: Foreign key referencing a numeric role ID for an applicant's desired position
- *        profile_id:
- *          type: string
- *          description: Foreign key referencing the applicant's profile ID
- *        approved:
- *          type: boolean
- *          description: Status of whether or not an application has been approved - defaults to false
- *        created_at:
- *          type: timestamp
- *          description: Record of an application's creation time
- *      example:
- *        application_id: 1
- *        position: 4
- *        profile_id: "10"
- *        approved: false
- *        created_at: "2021-11-01T17:59:02.023Z"
- */
+ *    Mentor example:
+ *      {
+      "profile_id": "805ff5x334yBv7Og",
+      "first_name": "Zavier",
+      "last_name": "Bailey",
+      "email": "Zavier.Bailey@gmail.com",
+      "country": "U.S.",
+      "state": "New Mexico",
+      "city": "New Town",
+      "current_company": "Uber",
+      "current_position": "Data Scientist",
+      "tech_stack": [
+        "iOS"
+      ],
+      "commitment": true,
+      "job_help": true,
+      "industry_knowledge": true,
+      "pair_programming": true,
+      "referred_by": "Family",
+      "other_info": "anything else may be written here",
+      "validate_status": "pending",
+      "is_active": true,
+      "accepting_new_mentees": true,
+      "created_at": "2022-08-26T15:15:48.109000"
+    }
+
+    Mentee example:
+      {
+      "profile_id": "j0346WB7lMj10kk8",
+      "first_name": "Nathan",
+      "last_name": "Gomez",
+      "email": "Nathan.Gomez@gmail.com",
+      "country": "U.S.",
+      "state": "of Columbia",
+      "city": "Washington",
+      "formerly_incarcerated": true,
+      "underrepresented_group": false,
+      "low_income": true,
+      "convictions": "Felony, Misdemeanor",
+      "tech_stack": "Design UI/UX",
+      "job_help": false,
+      "pair_programming": false,
+      "referred_by": "Friend",
+      "other_info": "anything else may be written here",
+      "validate_status": "approved",
+      "is_active": true,
+      "in_project_underdog": false,
+      "created_at": "2022-08-26T15:15:42.401000",
+      "updated_at": "2022-08-29T17:45:42.331000"
+    }
+
 
 /**
  * @swagger
  * /application:
- *  get:
- *    summary: Get the list of all pending applications
- *    description: Provides a JSON array of applications (as objects) where 'approved' key is falsy
+ *  post:
+ *    summary: Get the list of all applications (mentor and mentees)
+ *    description: Provides a JSON array of applications (as objects) for FE to consume and filter.
  *    tags:
  *      - application
  *    security:
- *      - okta: [authRequired, adminRequired]
+ *      - auth0: [authRequired, adminRequired]
  *    parameters:
  *      - in: query
  *        name: application property
  *        schema:
- *          type: string
- *        description: A resource property key to query for - accepts partial matching
+ *          type: object
+ *        description: A resource property key to query for
  *    responses:
  *      '200':
- *        description: An array of application objects
+ *        description: An array of application objects from MongoDB
  *        content:
  *          application/json:
  *            schema:
  *              type: array
- *              items:
- *                $ref: '#/components/schemas/Application'
- *              example:
- *                - profile_id: "00u13omswyZM1xVya4x7"
- *                  first_name: "User"
- *                  last_name: "6"
- *                  role_name: "mentor"
- *                  created_at: "2022-02-02T18:43:53.607Z"
- *                  application_id: 5
- *                - profile_id: "10"
- *                  first_name: "User"
- *                  last_name: "10"
- *                  role_name: "mentee"
- *                  created_at: "2022-02-02T18:43:53.607Z"
- *                  application_id: 6
- *      '401':
+ *              example: see above for mentee and mentor examples
+ *      '422':
  *        $ref: '#/components/responses/UnauthorizedError'
  */
 
 /*
   Author: Melody McClure
   This post route will read the 'readAllUsers' middleware and send back only the users who have applications in a pending validation status.
+  authRequired, and AdminRequired imports have been left (commented out) because they will likely be needed when auth0 is implemented.
 */
 router.post('/', readAllUsers, (req, res) => {
-  res.status(200).jsonsend(req.info);
-});
-
-/**
- * @swagger
- * /application/{role}:
- *  get:
- *    summary: Get the list of pending applicants by role name
- *    description: Provides a JSON array of applications (as objects) where 'approved' key is falsy
- *    tags:
- *      - application
- *    security:
- *      - okta: [authRequired, adminRequired]
- *    parameters:
- *      - in: param
- *        name: role name
- *        schema:
- *          type: string
- *        description: A request parameter that accepts 'mentor' or 'mentee'
- *    responses:
- *      '200':
- *        description: An array of application objects
- *        content:
- *          application/json:
- *            schema:
- *              type: array
- *              items:
- *                $ref: '#/components/schemas/Application'
- *              example:
- *                - profile_id: "00u13omswyZM1xVya4x7"
- *                  first_name: "User"
- *                  last_name: "6"
- *                  role_name: "mentor"
- *                  created_at: "2022-02-02T18:43:53.607Z"
- *                  application_id: 5
- *      '401':
- *        $ref: '#/components/responses/UnauthorizedError'
- */
-
-// get pending application tickets by role
-//
-// router.get('/:role', authRequired, adminRequired, (req, res) => {
-//   if (req.params.role === 'mentor') {
-//     res.json([
-//       {
-//         profile_id: '5b2t85faI2n133TM',
-//         first_name: 'User',
-//         last_name: '6',
-//         role_name: 'mentor',
-//         created_at: '2022-03-11T22:34:47.794Z',
-//         application_id: 5,
-//       },
-//     ]);
-//   } else {
-//     res.json([
-//       {
-//         profile_id: '506rV06k7cT8meR4',
-//         first_name: 'User',
-//         last_name: '8',
-//         role_name: 'mentee',
-//         created_at: '2022-03-11T22:34:47.794Z',
-//         application_id: 2,
-//         low_income: false,
-//       },
-//       {
-//         profile_id: '10',
-//         first_name: 'User',
-//         last_name: '10',
-//         role_name: 'mentee',
-//         created_at: '2022-03-11T22:34:47.794Z',
-//         application_id: 6,
-//       },
-//       {
-//         profile_id: '5b2t85faI2n133TM',
-//         first_name: 'User',
-//         last_name: '10',
-//         role_name: 'mentee',
-//         created_at: '2022-03-11T22:34:47.794Z',
-//         application_id: 6,
-//         validate_status: 'pending',
-//       },
-//     ]);
-//   }
-// });
-
-/**
- * @swagger
- * /application/{profileId/:id}:
- *  get:
- *    summary: Get the list of pending applicants by profile ID
- *    description: Provides a JSON array of applications (as objects) where 'approved' key is falsy
- *    tags:
- *      - application
- *    security:
- *      - okta: [authRequired, adminRequired]
- *    parameters:
- *      - in: param
- *        name: profile ID
- *        schema:
- *          type: string
- *        description: A request parameter that accepts profile ID
- *    responses:
- *      '200':
- *        description: An array of application objects
- *        content:
- *          application/json:
- *            schema:
- *              type: array
- *              items:
- *                $ref: '#/components/schemas/Application'
- *              example:
- *                - profile_id: "00u13omswyZM1xVya4x7"
- *                  first_name: "User"
- *                  last_name: "6"
- *                  role_name: "mentor"
- *                  created_at: "2022-02-02T18:43:53.607Z"
- *                  application_id: 5
- *      '401':
- *        $ref: '#/components/responses/UnauthorizedError'
- */
-
-// get application by profile id
-
-router.get('/profileId/:id', checkApplicationExists, checkRole, (req, res) => {
-  res.status(200).json(req.intakeData);
+  const users = req.info;
+  res.status(200).json({ users });
 });
 
 /**
@@ -316,6 +201,9 @@ NOTE: FE discerns if the user is a mentor/mentee and sends back the appropriate 
 mentor = {validate_status & current_company }
 mentee = {validate_status}
 this endpoint can then send the validate status to the appropriate endpoint depending on whether the current_company is present or not
+  
+authRequired, and AdminRequired imports have been left (commented out) because they will likely be needed when auth0 is implemented.
+
 */
 router.post('/update-validate_status/:profile_id', (req, res, next) => {
   const isMentor = req.body.current_company;
@@ -344,130 +232,6 @@ router.post('/update-validate_status/:profile_id', (req, res, next) => {
         next({ status: res.status, message: err });
       });
   }
-});
-
-/**
- * @swagger
- * /application/{update-notes/:id}:
- *  put:
- *    summary: Updates application_notes field using the application id
- *    description: An application_notes field is updated when application_id and changes, from the req.body, are sent to the updateApplicationNotes function in the applicationModel. The updated note is returned on a successful update or a 404 if the application does not exist. The authRequired and adminRequired middleware functions keep this endpoint secure.
- *    tags:
- *      - application
- *    security:
- *      - okta: [authRequired, adminRequired]
- *    parameters:
- *      - in: param
- *        name: application id
- *        schema:
- *          type: string
- *        description: Application ID of pending applicant
- *    responses:
- *      '200':
- *        description: Response from successful put
- *        content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                application_notes:
- *                  description: Status of the request as a message
- *                  type: string
- *              items:
- *                $ref: '#/components/schemas/Application'
- *              example:
- *                application_notes: 'Example of the application_notes'
- *      '401':
- *        $ref: '#/components/responses/UnauthorizedError'
- */
-
-// updates applicants application_notes field
-
-router.put(
-  '/update-notes/:id',
-  authRequired,
-  adminRequired,
-  (req, res, next) => {
-    const application_id = req.params.id;
-    const application_notes = req.body.application_notes;
-    Application.updateApplicationNotes(application_id, application_notes)
-      .then((response) => {
-        if (response == 0)
-          return next({ message: 'Application does not exist', status: 404 });
-
-        res.status(200).json(response[0]);
-      })
-      .catch(next);
-  }
-);
-
-/**
- * @swagger
- * /application/intake/:role/:id}:
- *  put:
- *    summary: Gets mentor or mentee intake data using role and id
- *    description: Mentor or mentee intake data is retrieved through a get request, where role and profile_id are sent as params. The mentor or mentee correlating with the profile_id will be returned on a successful get or a 404 if the profile does not exist. The authRequired and adminRequired middleware functions keep this endpoint secure.
- *    tags:
- *      - application
- *    security:
- *      - okta: [authRequired, adminRequired]
- *    parameters:
- *      - in: param
- *        name: role / profile id
- *        schema:
- *          type: string
- *        description: Profile ID of pending applicant and role (Mentors or Mentees)
- *    responses:
- *      '200':
- *        description: Response from successful get
- *        content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                message:
- *                  description: Result of request as object
- *                  type: object
- *              items:
- *                $ref: '#/components/schemas/Application'
- *              example:
- *                message: {"result": [{"profile_id": "4F177Y8xr4Ap1q0y","first_name": "Raiden","last_name": "Nelson","email": "fake@email.com","city": "Ashland","state": "Oregon","country": "USA",
-"formerly_incarcerated": true,"underrepresented_group": true,"low_income": true,"list_convictions": ["Infraction"],"subject": "General Programming","experience_level": "Expert","job_help": false,"industry_knowledge": false,"pair_programming": false,"other_info": "Notes"}]}
- *      '401':
- *        $ref: '#/components/responses/UnauthorizedError'
- */
-
-// gets mentor or mentee application intake data from Data Science
-
-router.get(
-  '/intake/:role/:id',
-  authRequired,
-  adminRequired,
-  (req, res, next) => {
-    const profile_id = req.params.id;
-    const role = req.params.role;
-    axios
-      .post(`${baseURL}/${role}/read`, { profile_id: profile_id })
-      .then((res) => {
-        if (res.data.result.length === 0)
-          return next({ message: 'Profile does not exist', status: 404 });
-
-        next({ status: res.status, message: res.data });
-      })
-      .catch((err) => {
-        next({ status: res.status, message: err });
-      });
-  }
-);
-
-// gets mentee Subject by mentee's ID
-router.get('/subject/:id', authRequired, (req, res, next) => {
-  const menteeId = req.params.id;
-  Application.getMenteeSubject(menteeId)
-    .then((subject) => {
-      res.status(200).json(subject);
-    })
-    .catch(next);
 });
 
 module.exports = router;
