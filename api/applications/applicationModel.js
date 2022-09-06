b52ee7dc2c54b156d2fb9ce105695bbfd1ccd626
enhopkin@gmail.com <enhopkin@gmail.com>
2022-09-06 16:09:21 -0600

    returned commented out model methods for other branches that may still be on Postgres. To be cleaned up at later date

--- api/applications/applicationModel.js
+++ api/applications/applicationModel.js
@@ -1,69 +1,70 @@
 const axios = require('axios');
 const { baseURL } = require('../../config/dsConfig');
+const db = require('../../data/db-config');
 
-// const getTicketById = async (ticket_id) => {
-// return db.select('*').from('tickets').where({ ticket_id }).first();
-// };
+const getTicketById = async (ticket_id) => {
+  return db.select('*').from('tickets').where({ ticket_id }).first();
+};
 
-// const getMentorIntake = async (mentor_intake_id) => {
-// return db
-//   .select('*')
-//   .from('mentor_intake')
-//   .where({ mentor_intake_id })
-//   .first();
-// };
+const getMentorIntake = async (mentor_intake_id) => {
+  return db
+    .select('*')
+    .from('mentor_intake')
+    .where({ mentor_intake_id })
+    .first();
+};
 
-// const getMenteeIntake = async (mentee_intake_id) => {
-// return db
-//   .select('*')
-//   .from('mentee_intake')
-//   .where({ mentee_intake_id })
-//   .first();
-// };
+const getMenteeIntake = async (mentee_intake_id) => {
+  return db
+    .select('*')
+    .from('mentee_intake')
+    .where({ mentee_intake_id })
+    .first();
+};
 
-// const insertMentorIntake = async (newMentorApplication) => {
-// console.log(newMentorApplication);
-// const newMentorIntake = await db('mentor_intake').insert(
-//   newMentorApplication
-// );
-// return newMentorIntake;
-// };
+const insertMentorIntake = async (newMentorApplication) => {
+  console.log(newMentorApplication);
+  const newMentorIntake = await db('mentor_intake').insert(
+    newMentorApplication
+  );
+  return newMentorIntake;
+};
 
-// const insertMenteeIntake = async (newMenteeApplication) => {
-// const newMenteeIntake = await db('mentee_intake').insert(
-//   newMenteeApplication
-// );
-// return newMenteeIntake;
-// };
+const insertMenteeIntake = async (newMenteeApplication) => {
+  const newMenteeIntake = await db('mentee_intake').insert(
+    newMenteeApplication
+  );
+  return newMenteeIntake;
+};
 
-// const add = async (applicationTicket) => {
-// const newTicket = await db('tickets').insert(applicationTicket);
-// return newTicket;
-// };
+const add = async (applicationTicket) => {
+  const newTicket = await db('tickets').insert(applicationTicket);
+  return newTicket;
+};
 
-// const getMenteeSubject = async (mentee_id) => {
-// const subject = await db
-//   .select('subject')
-//   .from('mentee_intake')
-//   .where('mentee_intake_id', mentee_id);
-// return subject;
-// };
+const getMenteeSubject = async (mentee_id) => {
+  const subject = await db
+    .select('subject')
+    .from('mentee_intake')
+    .where('mentee_intake_id', mentee_id);
+  return subject;
+};
 
 //currently (4/22), there is no 'applications' table; these notes instead exist as a column on the 'tickets' table. These will need to be updated if we do create a dedicated applications table!
-// const updateApplicationNotes = async (application_id, application_notes) => {
-// const notes = await db
-//   .select('tickets')
-//   .where('ticket_id', application_id)
-//   .insert('notes', application_notes);
-// return notes;
-// };
+const updateApplicationNotes = async (application_id, application_notes) => {
+  const notes = await db
+    .select('tickets')
+    .where('ticket_id', application_id)
+    .insert('notes', application_notes);
+  return notes;
+};
 
-// const updateTicket = async (application_id) => {
-// const result = await db('tickets')
-//   .where(application_id)
-//   .update({ approved: true });
-// return result;
-// };
+const updateTicket = async (application_id) => {
+  const result = await db('tickets')
+    .where(application_id)
+    .update({ approved: true });
+  return result;
+};
 
 //tag the mentors/mentees during this search.
 const getAllApplications = async (query) => {
@@ -92,14 +93,14 @@ const getAllApplications = async (query) => {
 };
 
 module.exports = {
-  // getTicketById,
-  // getMentorIntake,
-  // getMenteeIntake,
-  // insertMentorIntake,
-  // insertMenteeIntake,
-  // add,
-  // getMenteeSubject,
-  // updateApplicationNotes,
-  // updateTicket,
+  getTicketById,
+  getMentorIntake,
+  getMenteeIntake,
+  insertMentorIntake,
+  insertMenteeIntake,
+  add,
+  getMenteeSubject,
+  updateApplicationNotes,
+  updateTicket,
   getAllApplications,
 };
