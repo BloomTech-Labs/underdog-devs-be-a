a9caebcfa94859846f701b1ee957605c7ec4e8fc
Tim Sansone <tim.sansone@Tim-Sansone.local>
2022-10-10 15:03:04 -0500

    BL-849 updated migration for postgres profile table

--- data/migrations/20211026100908_profiles.js
+++ data/migrations/20211026100908_profiles.js
@@ -1,27 +1,10 @@
 exports.up = (knex) => {
   return knex.schema
-    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
     .createTable('profiles', function (table) {
-      table.string('profile_id').notNullable().unique().primary();
-      table.string('email').notNullable().unique();
-      table.string('first_name');
-      table.string('last_name');
-      table.string('location');
-      table.string('company');
-      table.specificType('tech_stack', 'text ARRAY');
-      table
-        .integer('role_id')
-        .unsigned()
-        .notNullable()
-        .default(5)
-        .references('role_id')
-        .inTable('roles')
-        .onDelete('CASCADE')
-        .onUpdate('CASCADE');
+      table.string('user_id').notNullable().unique().primary();
+      table.string('profile_id').notNullable().unique();
+      table.string('role').notNullable();
       table.timestamps(true, true);
-      table.boolean('is_active');
-      table.string('progress_status');
-      table.float('attendance_rate').notNullable().unsigned().default(1.0);
     });
 };
 
