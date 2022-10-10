e4e7d0da91ddf5f6f8195cc0a05c8125fe25bd3f
Tim Sansone <tim.sansone@Tim-Sansone.local>
2022-10-10 13:23:03 -0500

    BL-849 refactored passwordGenerator to meet Auth0 minimum password requirements

--- api/helpers/passGenerator.js
+++ api/helpers/passGenerator.js
@@ -1,10 +1,16 @@
 function passGenerator() {
   let password = '';
-  const str =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$!%^&*()';
-  for (let i = 0; i < 7; i++) {
-    const randNum = Math.floor(Math.random() * str.length);
-    password += str.charAt(randNum);
+  const chars = [
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    '0123456789',
+    'abcdefghijklmnopqrstuvwxyz',
+    '!@#$%^&*()',
+  ];
+  for (let i = 0; i < 4; i++) {
+    const randNum1 = Math.floor(Math.random() * chars[i].length);
+    const randNum2 = Math.floor(Math.random() * chars[i].length);
+    password += chars[i].charAt(randNum1);
+    password += chars[i].charAt(randNum2);
   }
   return password;
 }
