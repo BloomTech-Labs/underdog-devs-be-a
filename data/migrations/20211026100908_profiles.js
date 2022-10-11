299d3cb83aac65f90f2d129c4f42dbd89c15f2c9
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-10-11 12:37:08 -0700

    BL-849 Fixed linting warnings in new profiles table migration

--- data/migrations/20211026100908_profiles.js
+++ data/migrations/20211026100908_profiles.js
@@ -1,11 +1,10 @@
 exports.up = (knex) => {
-  return knex.schema
-    .createTable('profiles', function (table) {
-      table.string('user_id').notNullable().unique().primary();
-      table.string('profile_id').notNullable().unique();
-      table.string('role').notNullable();
-      table.timestamps(true, true);
-    });
+  return knex.schema.createTable('profiles', function (table) {
+    table.string('user_id').notNullable().unique().primary();
+    table.string('profile_id').notNullable().unique();
+    table.string('role').notNullable();
+    table.timestamps(true, true);
+  });
 };
 
 exports.down = (knex) => {
