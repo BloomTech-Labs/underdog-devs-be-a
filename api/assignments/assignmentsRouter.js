7206b126e10903f9aa1ce9fa2171def8157c6404
Tim Sansone <tim.sansone@Tim-Sansone.local>
2022-10-14 15:40:20 -0500

    BL-971 removed all unecessary endpoints and middleware

--- api/assignments/assignmentsRouter.js
+++ api/assignments/assignmentsRouter.js
@@ -20,145 +20,23 @@ router.get('/', authRequired, adminRequired, (req, res, next) => {
 
 // get all assignments by profile ID
 
-router.get('/:profile_id', authRequired, (req, res, next) => {
-  const { profile_id } = req.params;
-  const { role } = req.body;
+router.get('/:role/:profile_id', (req, res, next) => {
+  const { role, profile_id } = req.params;
   const payload = {
     user_id: profile_id,
     user_type: role,
   };
+
   axios
     .post(`${baseURL}/read/match`, payload)
     .then((dsRes) => {
       const results = dsRes.data[role === 'mentor' ? 'mentee' : 'mentor'];
       res.json(results);
     })
-    .catch(next);
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
 });
-
-// create a new assignment between a mentor and mentee
-
-router.post(
-  '/',
-  authRequired,
-  validNewAssign,
-  adminRequired,
-  (req, res, next) => {
-    const assignment = req.body;
-    Assignment.Create(assignment)
-      .then(() => {
-        res.status(201).json({ message: 'success' });
-      })
-      .catch(next);
-  }
-);
-
-// update a assignment by assignment id, must be real mentee/mentor id
-
-router.put(
-  '/:id',
-  authRequired,
-  validAssignID,
-  adminRequired,
-  (req, res, next) => {
-    const id = req.params.id;
-    const changes = req.body;
-    Assignment.Update(id, changes)
-      .then((change) => {
-        if (change === 1) {
-          Assignment.findById(id).then((success) => {
-            res.status(200).json({
-              message: `Assignment '${success.assignment_id}' updated`,
-              success,
-            });
-          });
-        }
-      })
-      .catch(next);
-  }
-);
-
-//delete Assignment by assignment_id
-
-router.delete(
-  '/:id',
-  authRequired,
-  validAssignID,
-  adminRequired,
-  (req, res, next) => {
-    const id = req.params.id;
-    Assignment.Remove(id)
-      .then((assignment) => {
-        if (assignment) {
-          res.status(200).json({
-            message: 'assignment deleted',
-          });
-        }
-      })
-      .catch(next);
-  }
-);
-
-// get assignment by assignment id
-
-router.get(
-  '/:id',
-  authRequired,
-  validAssignID,
-  adminRequired,
-  (req, res, next) => {
-    const id = req.params.id;
-    Assignment.findById(id)
-      .then((assigns) => {
-        res.status(200).json(assigns);
-      })
-      .catch((err) => {
-        next({ status: 500, message: err.message });
-      });
-  }
-);
-
-///////////////////////////MIDDLEWARE///////////////////////////////
-
-// Validate the Assignment_id Middleware
-
-function validAssignID(req, res, next) {
-  Assignment.findById(req.params.id)
-    .then((assignment) => {
-      if (assignment) {
-        req.assignment = assignment;
-        next();
-      } else {
-        next({
-          status: 404,
-          message: 'Invalid assignment ID',
-        });
-      }
-    })
-    .catch(next);
-}
-
-// validate new assignment includes both mentor_id and mentee_id
-
-function validNewAssign(req, res, next) {
-  const assign = req.body;
-  if (!assign) {
-    res.status(400).json({
-      message: 'Missing Assignment Data',
-    });
-  } else if (!assign.mentor_id) {
-    next({
-      status: 400,
-      message: 'Missing mentor_id field',
-    });
-  } else if (!assign.mentee_id) {
-    next({
-      status: 400,
-      message: 'Missing mentee_id field',
-    });
-  } else {
-    next();
-  }
-}
 
 module.exports = router;
