be0e2fd1fdc07145381e9a175a09b3e3916dc52f
James <heartandkarma@gmail.com>
2022-10-26 11:40:30 -0700

    Finished fixing the migrations file. Seeds are updated.

--- data/migrations/20211108175854_meetings.js
+++ data/migrations/20211108175854_meetings.js
@@ -2,11 +2,11 @@ exports.up = function (knex) {
   return knex.schema
     .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
     .createTable('meetings', function (table) {
-      table.increments('meeting_id', 36).notNullable().unique().primary();
+      table.string('meeting_id', 36).notNullable().unique().primary();
       table.timestamps(true, true);
       table.string('meeting_topic', 255).notNullable();
-      table.integer('meeting_start_time').notNullable().unsigned();
-      table.integer('meeting_end_date').notNullable().unsigned();
+      table.dateTime('meeting_start_time').notNullable().unsigned();
+      table.dateTime('meeting_end_time').notNullable().unsigned();
       table
         .string('mentor_id', 36)
         .references('profile_id')
@@ -23,7 +23,7 @@ exports.up = function (knex) {
         .notNullable();
       table.string('admin_meeting_notes', 2000).defaultTo(null);
       table
-        .enu('meeting_missed', ['Missed', 'Pending', 'Attended'])
+        .enu('meeting_missed_by_mentee', ['Missed', 'Attended'])
         .notNullable()
         .defaultTo('Pending');
       table.string('mentor_meeting_notes', 2000).defaultTo(null);
