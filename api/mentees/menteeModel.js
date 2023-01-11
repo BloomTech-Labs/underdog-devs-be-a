29534df64544c416b0b7561faf725d84a9db238c
jesswillcode <jesswillcode@gmail.com>
2023-01-11 17:59:21 -0500

    creating model and router
    Co-authored-by: Jess Williams <jesswillcode@gmail.com>
    Co-authored-by: Branden Jones <branden.wayne.jones@gmail.com>

---
+++ api/mentees/menteeModel.js
@@ -0,0 +1,32 @@
+// const db = require('../../data/db-config');
+
+//findAll
+// const findAll = () => {
+//   return db('mentees');
+// };
+
+//findBy
+
+//findById
+
+//create
+
+//update
+
+//delete
+
+//What Mentors & Admin need
+//first_name
+//last_name
+//email
+//tech_stack
+//validate_status
+
+// module.exports = {
+//   //   findAll,
+//   //   findBy,
+//   //   findById,
+//   //   create,
+//   //   update,
+//   //   remove,
+// };
