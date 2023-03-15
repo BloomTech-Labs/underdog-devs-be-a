2155c3f31330d4289d91a66ebc16dd0bf463ac66
Branden Jones <branden.wayne.jones@gmail.com>
2023-03-15 13:13:41 -0400

    removed console logs

--- api/analytics/graphRouter.js
+++ api/analytics/graphRouter.js
@@ -3,14 +3,11 @@ const router = express.Router();
 const axios = require('axios');
 const { baseURL } = require('../../config/dsConfig');
 
-console.log(`GraphRouter`);
-
 router.get('/graph/tech', (req, res) => {
   console.log(`Hello from ENDPOINT`, baseURL);
   axios
     .get(`${baseURL}/graph/tech-stack-by-role`)
     .then((resp) => {
-      console.log(resp);
       res.status(200).send(resp.data);
     })
     .catch((err) => console.error(err));
