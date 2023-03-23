db2c1eed25d3e67b0a2dc69ca7676b8553e981ab
Branden Jones <branden.wayne.jones@gmail.com>
2023-03-23 15:25:58 -0400

    router grabs mentee or mentor

--- api/userManagement/userManagementRouter.js
+++ api/userManagement/userManagementRouter.js
@@ -3,11 +3,26 @@ const router = express.Router();
 const axios = require('axios');
 const { baseURL } = require('../../config/dsConfig');
 
-router.get('/get/all', (req, res) => {
-  axios
-    .get(`${baseURL}/get/all`)
+router.post('/read/mentor', (req, res) => {
+  axios({
+    method: 'post',
+    url: `${baseURL}/read/mentor`,
+    data: {},
+  })
     .then((resp) => {
-      res.status(200).json(resp.data);
+      res.status(200).json(resp.data.result);
+    })
+    .catch((err) => console.error(err));
+});
+
+router.post('/read/mentee', (req, res) => {
+  axios({
+    method: 'post',
+    url: `${baseURL}/read/mentee`,
+    data: {},
+  })
+    .then((resp) => {
+      res.status(200).json(resp.data.result);
     })
     .catch((err) => console.error(err));
 });
