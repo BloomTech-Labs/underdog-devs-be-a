7ec78003c93783a91c5d0d651f09e94e64ab0213
Branden Jones <branden.wayne.jones@gmail.com>
2023-04-03 11:08:00 -0400

    add update to matching Router

--- api/matchingModal/matchingModalRouter.js
+++ api/matchingModal/matchingModalRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const axios = require('axios');
 const { baseURL } = require('../../config/dsConfig');
 
+// profile objects
 router.post('/read/:role', (req, res) => {
   const role = req.params.role;
   axios({
@@ -16,6 +17,7 @@ router.post('/read/:role', (req, res) => {
     .catch((err) => console.error(err));
 });
 
+// suggested matches
 router.post('/:role/:profile_id', (req, res) => {
   const role = req.params.role;
   const profile_id = req.params.profile_id;
@@ -25,6 +27,24 @@ router.post('/:role/:profile_id', (req, res) => {
   })
     .then((resp) => {
       res.status(200).json(resp.data.result);
+    })
+    .catch((err) => console.error(err));
+});
+
+// update matches
+router.patch('/update/:role/:profileID/', (req, res) => {
+  const profile_id = req.params.profileID;
+  const role = req.params.role;
+  const newArray = req.body;
+  axios({
+    method: 'patch',
+    url: `${baseURL}/update/${role}/${profile_id}`,
+    data: {
+      matches: newArray,
+    },
+  })
+    .then((resp) => {
+      res.status(200).json(resp.response);
     })
     .catch((err) => console.error(err));
 });
