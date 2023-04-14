79c9413601979236502416e8b43121f2248919bb
jesswillcode <jesswillcode@gmail.com>
2023-04-14 11:43:55 -0400

    fixed vars

--- api/applications/applicationRouter.js
+++ api/applications/applicationRouter.js
@@ -10,10 +10,6 @@ const { v4: uuidv4 } = require('uuid');
 const validation = require('../helpers/validation');
 const axios = require('axios');
 const { baseURL } = require('../../config/dsConfig');
-const { issuer, connection, token } = require('../../config/auth0');
-const { passGenerator } = require('../helpers/passGenerator');
-const Profiles = require('../profile/profileModel');
-
 /**
  * @swagger
  * components:
@@ -292,68 +288,24 @@ router.post('/new/:role', validation(), async (req, res, next) => {
  */
 
 router.post('/update-validate_status/:profile_id', async (req, res, next) => {
-  const { role, email, status } = req.body;
+  const { role, validate_status } = req.body;
   const { profile_id } = req.params;
-  let user_id;
 
-  try {
-    const statusUpdate = await axios.post(
-      `${baseURL}/update/${role}/${profile_id}`,
-      {
-        validate_status: status,
-      }
-    );
-    if (status === 'rejected') {
-      res.json({ status: 200, message: `${role} rejected!` });
-      return statusUpdate;
-    } else {
-      res.json({ status: 200, message: `${role} accepted!` });
-      return statusUpdate;
-    }
-  } catch (err) {
-    next({
-      status: err.response.status,
-      message: err.response.data.detail[0].msg,
+  axios
+    .patch(`${baseURL}update/${role}/${profile_id}`, {
+      validate_status: validate_status,
+    })
+    // eslint-disable-next-line no-unused-vars
+    .then((resp) => {
+      res.status(200).json({
+        message: `${role} ${validate_status}!`,
+      });
+    })
+    .catch((err) => {
+      next({
+        message: err.response,
+      });
     });
-  }
-
-  try {
-    const payload = {
-      email,
-      password: passGenerator(8),
-      connection,
-    };
-    const authData = await axios.post(`${issuer}api/v2/users`, payload, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    user_id = authData.data.identities[0].user_id;
-  } catch (err) {
-    next({
-      status: err.response.data.statusCode,
-      message: `${err.response.data.error}: ${err.response.data.message}`,
-    });
-    return;
-  }
-
-  try {
-    const newProfile = {
-      user_id,
-      profile_id,
-      role,
-    };
-
-    await Profiles.create(newProfile);
-  } catch (err) {
-    next({ status: 422, message: err.message });
-    return;
-  }
-
-  res.json({
-    status: 201,
-    message: `${role} application approved and user created!`,
-  });
 });
 
 module.exports = router;
